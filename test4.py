eec4f8a56ff026e5c9c270fd10548440945a1418
nguyen quang hoang <hoangdev161201@gmail.com>
2025-09-24 10:04:09 +0700

    upgrade

--- test4.py
+++
@@ -1,9 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key='AIzaSyAWzlnFgRmr_6nddTqQeJgSr1DIPb32W6M')
-
-models = genai.list_models()
-for m in models:
-    # bạn có thể lọc những model có hỗ trợ generateContent
-    if 'generateContent' in m.supported_generation_methods:
-        print(m.name, m.description, m.supported_generation_methods)
